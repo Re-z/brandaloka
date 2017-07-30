5d7fce77aa337d1c0b23b611a1cc00f3baca48ce
Re-z <re-z@ukr.net>
2017-07-30 18:46:55 +0300

    scrollr test

--- app/js/common.js
+++ app/js/common.js
@@ -38,82 +38,51 @@ $(document).ready(function() {
         $('.js-clients-slider').slick('slickPrev')
     });
 
-
-
-
-
-
-    let loadPosts = () => {
-        let trigger = document.querySelector('.more-posts');
-        trigger.addEventListener('click', () => {
-           let xhr = new XMLHttpRequest();
-            xhr.open('GET','../posts.json');
-            xhr.addEventListener('load', () => {
-                let response = JSON.parse(xhr.responseText);
-                let article = document.querySelector('.posts__item');
-                let posts = document.querySelector('.posts');
-
-                for(let i = 0; i<response.length; i++) {
-                    let newArticle = article.cloneNode(true);
-
-                    //changing properties according to json
-                    newArticle.querySelector('img').src = response[i].img;
-                    newArticle.querySelector('a').href = response[i].href;
-                    newArticle.querySelector('h5').innerHTML = response[i].title;
-                    newArticle.querySelector('p').innerHTML = response[i].text;
-                    newArticle.querySelector('time').innerHTML = response[i].time;
-                    newArticle.querySelector('span').innerHTML = response[i].comments;
-
-                    //add new posts
-                    posts.appendChild(newArticle);
-                }
-            })
-            xhr.send();
-        })
-    }
-    loadPosts();
-
-    //mobile burger
     let burger = document.querySelector('.header__burger');
     let menu = document.querySelector('.mob-menu');
     let close = document.querySelector('.mob-menu__close');
     burger.addEventListener('click', () => {
         menu.classList.add('is-active');
     });
-    close.addEventListener('click', () => {
-        menu.classList.remove('is-active');
+        close.addEventListener('click', () => {
+            menu.classList.remove('is-active');
     });
 
 
 
 
-    ////show skills
-    //let showSkillsOnScroll = () => {
-    //    window.addEventListener('scroll', function scroll(ev) {
-    //        if(window.pageYOffset > 5300) {
-    //        document.body.classList.add('no-scroll');
-    //        window.addEventListener('wheel', function showSkillsOnWheel(ev) {
-    //            let item = document.querySelector('.skills__item.is-hidden');
-    //            //var currCount = +ev.wheelDelta;
-    //            //
-    //            //var count;
-    //            //count +=currCount
-    //            //alert(count)
+
+
+    //let loadPosts = () => {
+    //    let trigger = document.querySelector('.more-posts');
+    //    trigger.addEventListener('click', () => {
+    //       let xhr = new XMLHttpRequest();
+    //        xhr.open('GET','../posts.json');
+    //        xhr.addEventListener('load', () => {
+    //            let response = JSON.parse(xhr.responseText);
+    //            let article = document.querySelector('.posts__item');
+    //            let posts = document.querySelector('.posts');
     //
+    //            for(let i = 0; i<response.length; i++) {
+    //                let newArticle = article.cloneNode(true);
     //
-    //            if(item) {
-    //                item.classList.remove('is-hidden')
-    //                }
-    //            else {
-    //                window.removeEventListener('scroll', scroll);
-    //                document.body.classList.remove('no-scroll');
+    //                //changing properties according to json
+    //                newArticle.querySelector('img').src = response[i].img;
+    //                newArticle.querySelector('a').href = response[i].href;
+    //                newArticle.querySelector('h5').innerHTML = response[i].title;
+    //                newArticle.querySelector('p').innerHTML = response[i].text;
+    //                newArticle.querySelector('time').innerHTML = response[i].time;
+    //                newArticle.querySelector('span').innerHTML = response[i].comments;
     //
+    //                //add new posts
+    //                posts.appendChild(newArticle);
     //            }
-    //
-    //            })
-    //    }
+    //        })
+    //        xhr.send();
     //    })
     //}
-    //showSkillsOnScroll();
+    //loadPosts();
+
+
 
 });
